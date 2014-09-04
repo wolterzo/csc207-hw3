c4f9a949adbc67a1fbe52f1943e3916079f2c375
wolterzo <wolterzo@grinnell.edu>
2014-09-03 20:35:33 -0500

    add java project files and stub methods.

---
+++ src/assignment/PartA.java
@@ -0,0 +1,59 @@
+package assignment;
+
+public class PartA
+{
+  /**
+   * Determine if a is a multiple of b.
+   */
+  public static boolean isMultiple(long a, long b)
+  {
+    // STUB
+    return true;
+  } // isMultiple(long, long)
+  
+  /**
+   * Determine if i is odd.
+   */
+  public static boolean isOdd(int i)
+  {
+    //STUB
+    return true;
+  } //isOdd(int)
+  
+  /**
+   * Produce sum on all positive odd integers less than n.
+   */
+  public static int oddSumTo(int n)
+  {
+    //STUB
+    return 0;
+  }//oddSumTo (int)
+  
+  /**
+   * Determine if any pair of integers in an array has a 
+   * product that is odd.
+   */
+  public static boolean isOddProd(int[] ints)
+  {
+    //STUB
+    return true;
+  }//isOddProd(int[])
+  
+  /**
+   * Determine if no two integers in an array have equal values.
+   */
+  public static boolean allDistinct(int[] ints)
+  {
+    //STUB
+    return true;
+  }//allDistinct(int[])
+  
+  /**
+   * Reverse the order of integers in an array.
+   */
+  public static void reverseInts(int[] ints)
+  {
+    //STUB
+  }//reverseInts(int[])
+  
+} //class PartA
