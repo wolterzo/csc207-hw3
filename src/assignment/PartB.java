a2f9a8418486b2077bfd029e6d3a539abfa18feb
wolterzo <wolterzo@grinnell.edu>
2014-09-03 21:37:35 -0500

    add complete PartB

---
+++ src/assignment/PartB.java
@@ -0,0 +1,18 @@
+package assignment;
+
+public class PartB
+{
+  /**
+   * Compute the average of two integers.  Rounds toward zero if the
+   * average is not a whole number.
+   */
+  public static int average(int left, int right) 
+  {
+    /* Here if you use MAX_VALUE, it gives an incorrect answer
+     * return ((left + right) / 2);
+     * */
+    return((left/2)+(right/2));
+    
+    
+  } // average(int,int)
+} // class PartB
