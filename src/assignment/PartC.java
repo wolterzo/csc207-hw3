9d4f1362367e68024f7632e0a66815b1be57b37c
wolterzo <wolterzo@grinnell.edu>
2014-09-04 23:00:53 -0500

    add possible methods for ushahidi incidents

---
+++ src/assignment/PartC.java
@@ -0,0 +1,70 @@
+package assignment;
+
+public class PartC
+{
+/*
+ * OBSERVERS
+ * Method:      getOwnerName
+   Type:        Observer
+   Purpose:     Get the name of the owner of this resource.
+   Parameters:  [none]
+   Produces:    name, a String
+   
+ * Method:      getIncidentName
+   Type:        Observer
+   Purpose:     Get the name incident reported.
+   Parameters:  [none]
+   Produces:    name, a String
+   
+ * Method:      getIncidentLocationName
+   Type:        Observer
+   Purpose:     Get the name of the location of the incident
+                reported.
+   Parameters:  [none]
+   Produces:    name, a String
+   
+ * Method:      getIncidentLocationCoordinates
+   Type:        Observer
+   Purpose:     Get the coordinates of the location of the incident
+                reported.
+   Parameters:  [none]
+   Produces:    coords, double[]
+      
+ * Method:      getIncidentDescription
+   Type:        Observer
+   Purpose:     Get the description of the incident reported.
+   Parameters:  [none]
+   Produces:    name, a String
+   
+ * Method:      getIncidentTags
+   Type:        Observer
+   Purpose:     Get the tags of the incident reported.
+   Parameters:  [none]
+   Produces:    tags, an array of strings
+ 
+ * MUTATORS
+ * Method:      addTag
+   Type:        Mutator
+   Purpose:     Add a tag to an existing incident
+   Parameters:  tag, a string
+   Produces:    [none]
+   
+ * Method:      updateDescription
+   Type:        Mutator
+   Purpose:     Change the description of the incident.
+   Parameters:  text, a string
+   Produces:    [none]
+   
+ * CONSTRUCTOR
+ * Method:      createIncident
+   Type:        Constructor
+   Purpose:     Create incident object.
+   Parameters:  title, string,
+                description, string,
+                locationName, string, 
+                coords, double[],
+                ownerName, string,
+                tags, string[],
+   Produces:    incident, an object
+ */
+}
